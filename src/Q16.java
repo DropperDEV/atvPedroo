9a84e4790590f907e79be89e89627e662c2da9a0
dropperDEV <jamescosta4747@Gmail.com>
2024-02-14 23:07:11 -0300

    feat: add 16 question

---
+++ src/Q16.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Q16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int) sc.nextDouble());
+        sc.close();
+    }
+}
